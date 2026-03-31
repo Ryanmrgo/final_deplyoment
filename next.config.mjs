b13526d89479fed601e28f58d5008f01de331e5c
Ryanmrgo <pyaesone150945@gmail.com>
2026-04-01 00:32:41 +0630

    skip eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Allow production builds to complete even with ESLint warnings/errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
